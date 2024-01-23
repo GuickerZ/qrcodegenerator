37513e1cfb10808835add3c77122091304972ce0
Guicker <80110265+GuickerZ@users.noreply.github.com>
2024-01-23 20:04:36 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const axios = require('axios');
+const path = require('path');
 const cors = require('cors');
 const app = express();
 const PORT = 3000;
-
+const PORT2 = 80;
 app.use(express.json());
 app.use(cors({origin: "*"}))
 app.post('/generateQRCode', async (req, res) => {
@@ -19,4 +20,11 @@ app.post('/generateQRCode', async (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT2, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT2}`);
 });
